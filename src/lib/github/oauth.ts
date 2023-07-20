5d2933c6edd2793a1f5a9af38e65f9c83257df8b
Robert Vargas <robvargas@google.com>
2023-07-20 11:08:30 -0700

    added auth storage

---
+++ src/lib/github/oauth.ts
@@ -0,0 +1,41 @@
+import { App } from "octokit";
+import GITHUB_KEY from '../../../.env.private-key.pem?raw';
+import { requireEnv } from "../env";
+import { GitHubAccessTokenResponse } from "~/routes/oauth/callback";
+
+type AuthCode = string;
+const authorizedUsers = new Map<AuthCode, GitHubAccessTokenResponse>();
+
+export function getAuthUrl(): string {
+  const GITHUB_APP_ID = requireEnv('GITHUB_APP_ID');
+  const GITHUB_CLIENT_ID = requireEnv('GITHUB_CLIENT_ID');
+  const GITHUB_CLIENT_SECRET = requireEnv('GITHUB_CLIENT_SECRET');
+  const app = new App({
+    appId: GITHUB_APP_ID,
+    privateKey: GITHUB_KEY,
+    oauth: { clientId: GITHUB_CLIENT_ID, clientSecret: GITHUB_CLIENT_SECRET }
+  });
+  const res = app.oauth.getWebFlowAuthorizationUrl({});
+  return res.url;
+}
+
+export function registerUserAuthPackage(code: AuthCode, githubAuthResponse: GitHubAccessTokenResponse): void {
+  authorizedUsers.set(code, githubAuthResponse);
+  console.log({authorizedUsers});
+}
+
+export function deleteUserAuthPackage(code: AuthCode): boolean {
+  return authorizedUsers.delete(code);
+}
+
+export function getAccessToken(code: AuthCode): GitHubAccessTokenResponse['access_token'] {
+  const authResponse = authorizedUsers.get(code);
+  if (!authResponse) {
+    throw new Error(`No GitHub access authentication found for code ${code}.`);
+  }
+  return authResponse.access_token;
+}
+
+export function isAuthorized(code: AuthCode): boolean {
+  return authorizedUsers.has(code);
+}
