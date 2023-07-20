419fa807ccbf05f77545f53c4dde3290720cc5fd
Robert Vargas <robvargas@google.com>
2023-07-20 16:50:31 -0700

    login button is now reactive

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -1,33 +1,61 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useSignal, useTask$ } from "@builder.io/qwik";
 import { GitHubLogo } from "../starter/icons/github";
-import { getAuthUrl } from "~/lib/github/oauth";
+import { getGitHubAuthUrl, getGitHubToken } from "~/lib/github/oauth";
+import { server$ } from "@builder.io/qwik-city";
+import { GitHubUser, fetchUserInfoFromAuth } from "~/lib/github/user";
 
-const AuthUrl = getAuthUrl();
+const fetchUser = server$(async function () {
+  console.log('FETCHUSER CALLED');
+  const oauth = this.cookie.get('oauth');
+  if (!oauth) {
+    return;
+  }
+
+  let token;
+  try {
+    token = getGitHubToken(oauth.value);
+  }
+  catch (e: unknown) {
+    return;
+  }
+
+  return fetchUserInfoFromAuth(token);
+});
 
 /**
  * Button to redirect a user to Github OAuth2 or logout
  */
 export const Login = component$(() => {
-  const loggedIn = false; // TODO: Implement signal to check if authorized
+  const user = useSignal<GitHubUser>();
 
-  return loggedIn ?
-    <a href={AuthUrl}>
+  useTask$(async () => {
+    user.value = await fetchUser();
+  })
+
+  return (
+    <a href={!!user.value ? '/logout' : getGitHubAuthUrl()}>
       <button
         style={{
           display: "flex",
           alignItems: "center",
+          alignContent: "center",
           justifyContent: "space-between",
           gap: "1em",
+          maxHeight: '4em'
         }}
       >
-        <div>Login with GitHub</div>
-        <div>
-          <GitHubLogo height={25} width={25} />
+        <div>{
+          !!user.value
+            ? `Log out of ${user.value.login}`
+            : 'Login with GitHub'
+        }
         </div>
+        <div>{
+          !!user.value
+            ? <img src={user.value.avatarUrl} alt="" style={{ borderRadius: '50%', width: '40px', height: '40px' }} />
+            : <GitHubLogo height={25} width={25} />
+        }</div>
       </button>
     </a>
-    :
-    <a href={'/logout'}>
-      <button>Sign Out</button>
-    </a>
+  )
 });
