b1d4e60d755dd9f616b8dbdf2592e91616ca2a97
Robert Vargas <robvargas@google.com>
2023-07-20 11:14:10 -0700

    oauth/callback endpoint trades codes to auth

--- src/routes/oauth/callback/index.tsx
+++ src/routes/oauth/callback/index.tsx
@@ -1,6 +1,17 @@
 import { RequestHandler } from '@builder.io/qwik-city';
+import { registerUserAuthPackage, tradeCodeForAuthPackage } from '~/lib/github/oauth';
 
 export const onGet: RequestHandler = async function ({ query, cookie, redirect }) {
-  cookie.set('oauth', query.get('code') ?? '', { path: '/' });
+  const code = query.get('code');
+  console.log({ code });
+  if (!code) {
+    alert('No GitHub authorization code received. Unable to authorize.');
+    throw redirect(307, '/');
+  }
+
+  const authPackage = await tradeCodeForAuthPackage(code);
+  registerUserAuthPackage(code, authPackage);
+  cookie.set('oauth', code, { path: '/' });
+
   throw redirect(307, '/');
 }
