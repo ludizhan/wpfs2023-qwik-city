e9f39a1023e3533f8ed66fc2f956a60d48e80244
Robert Vargas <robvargas@google.com>
2023-07-20 11:08:40 -0700

    updated with auth functions

--- src/routes/logout.tsx
+++ src/routes/logout.tsx
@@ -1,6 +1,11 @@
 import { RequestHandler } from '@builder.io/qwik-city';
+import { deleteUserAuthPackage } from '~/lib/github/oauth';
 
 export const onGet: RequestHandler = async function ({ cookie, redirect }) {
-  cookie.delete('oauth');
+  const code = cookie.get('oauth');
+  if (code) {
+    cookie.delete('oauth');
+    deleteUserAuthPackage(code.value);
+  }
   throw redirect(307, '/');
 }
