d5ae8a50c3345a4d13ab954a32be5a870be54184
Robert Vargas <robvargas@google.com>
2023-07-19 18:31:17 -0700

    added login button

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -12,24 +12,24 @@ interface UserLoginButtonProps {
  * Button to redirect a user to Github OAuth2
  */
 export const Login = component$(({ loggedIn }: UserLoginButtonProps) => {
-  return loggedIn ? 
-          <a href={AuthUrl}>
-            <button
-              style={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "space-between",
-                gap: "1em",
-              }}
-            >
-              <div>Login with GitHub</div>
-              <div>
-                <GitHubLogo height={25} width={25} />
-              </div>
-            </button>
-          </a>
-        :
-          <a>
-            <button onClick$={() => { console.error('Sign Out Button: Implement me.') }}>Sign Out</button>
-          </a>
+  return loggedIn ?
+    <a href={AuthUrl}>
+      <button
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          gap: "1em",
+        }}
+      >
+        <div>Login with GitHub</div>
+        <div>
+          <GitHubLogo height={25} width={25} />
+        </div>
+      </button>
+    </a>
+    :
+    <a>
+      <button onClick$={() => { console.error('Sign Out Button: Implement me.') }}>Sign Out</button>
+    </a>
 });
