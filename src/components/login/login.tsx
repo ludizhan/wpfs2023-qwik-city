ac7aa0ad39c11f150b393150d00750597e2e5571
Robert Vargas <robvargas@google.com>
2023-07-19 18:31:08 -0700

    added login button

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -1,26 +1,35 @@
 import { component$ } from "@builder.io/qwik";
 import { GitHubLogo } from "../starter/icons/github";
-import { GitHubOAuthLink } from "~/lib/github/oauth";
+import { getAuthUrl } from "~/lib/github/discussions";
+
+const AuthUrl = getAuthUrl();
+
+interface UserLoginButtonProps {
+  loggedIn: boolean;
+}
 
 /**
  * Button to redirect a user to Github OAuth2
  */
-export const Login = component$(() => {
-  return (
-    <a href={GitHubOAuthLink}>
-      <button
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "space-between",
-          gap: "1em",
-        }}
-      >
-        <div>Login with GitHub</div>
-        <div>
-          <GitHubLogo height={25} width={25} />
-        </div>
-      </button>
-    </a>
-  );
+export const Login = component$(({ loggedIn }: UserLoginButtonProps) => {
+  return loggedIn ? 
+          <a href={AuthUrl}>
+            <button
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-between",
+                gap: "1em",
+              }}
+            >
+              <div>Login with GitHub</div>
+              <div>
+                <GitHubLogo height={25} width={25} />
+              </div>
+            </button>
+          </a>
+        :
+          <a>
+            <button onClick$={() => { console.error('Sign Out Button: Implement me.') }}>Sign Out</button>
+          </a>
 });
